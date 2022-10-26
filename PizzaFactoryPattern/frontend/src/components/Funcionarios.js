034d1941b365cdfbd78b0e5c91b38b387f0038fb
pedro.velosa <pedro.velosa@fiap.com.br>
2022-10-26 18:26:12 -0300

    aba funcionarios finalizada

--- PizzaFactoryPattern/frontend/src/components/Funcionarios.js
+++ PizzaFactoryPattern/frontend/src/components/Funcionarios.js
@@ -1,35 +1,72 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Funcionarios = () => {
   return (
     <div className='pt-8 px-16'>
       <h1 className='text-3xl font-bold mb-8'>Funcionarios</h1>
       <div className='flex flex-col gap-4'>
-        <article>
-          <img src="" />
-          <h2 className='text-2xl'>Pedro</h2>
-          <h3 className='text-xl'>Rm84591</h3>
+        <article className='flex gap-4 items-center mb-8 ml-8'>
+          <Image
+            src={'/assets/pedro.jpeg'}
+            width={'75px'}
+            height={'75px'}
+            className={'rounded-full'}
+          />
+          <div>
+            <h2 className='text-2xl'>Pedro</h2>
+            <h3 className='text-xl'>RM84591</h3>
+          </div>
         </article>
-        <article>
-          <img src="" />
-          <h2 className='text-2xl'>Pedro</h2>
-          <h3 className='text-xl'>Rm84591</h3>
+        <article className='flex gap-4 items-center mb-8 ml-8'>
+          <Image
+            src={'/assets/eduardo.jpeg'}
+            width={'75px'}
+            height={'75px'}
+            className={'rounded-full'}
+          />
+          <div>
+            <h2 className='text-2xl'>Eduardo</h2>
+            <h3 className='text-xl'>RM86407</h3>
+          </div>
         </article>
-        <article>
-          <img src="" />
-          <h2 className='text-2xl'>Pedro</h2>
-          <h3 className='text-xl'>Rm84591</h3>
+        <article className='flex gap-4 items-center mb-8 ml-8'>
+          <Image
+            src={'/assets/matheus.jpeg'}
+            width={'75px'}
+            height={'75px'}
+            className={'rounded-full'}
+          />
+          <div>
+            <h2 className='text-2xl'>Matheus</h2>
+            <h3 className='text-xl'>RM85695</h3>
+          </div>
         </article>
-        <article>
-          <img src="" />
-          <h2 className='text-2xl'>Pedro</h2>
-          <h3 className='text-xl'>Rm84591</h3>
+        <article className='flex gap-4 items-center mb-8 ml-8'>
+          <Image
+            src={'/assets/anissa.jpeg'}
+            width={'75px'}
+            height={'75px'}
+            className={'rounded-full'}
+          />
+          <div>
+            <h2 className='text-2xl'>Anissa</h2>
+            <h3 className='text-xl'>RM86478</h3>
+          </div>
         </article>
-        <article>
-          <img src="" />
-          <h2 className='text-2xl'>Pedro</h2>
-          <h3 className='text-xl'>Rm84591</h3>
+        <article className='flex gap-4 items-center mb-8 ml-8'>
+          <Image
+            src={'/assets/stella.jpg'}
+            width={'75px'}
+            height={'75px'}
+            className={'rounded-full'}
+          />
+          <div>
+            <h2 className='text-2xl'>Stella</h2>
+            <h3 className='text-xl'>RM85683</h3>
+          </div>
         </article>
+
       </div>
     </div>
   );
